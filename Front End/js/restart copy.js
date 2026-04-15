3b787484f32fc6a7ea7e825d77a8973e14964dc3
Ahmedvip62 <ahmed192r22@gmail.com>
2026-04-15 15:39:13 +0200

    Delete Front End directory

--- Front End/js/restart copy.js
+++
@@ -1,8 +0,0 @@
-﻿// --- Theme Logic (Matches user system) ---
-        const html = document.documentElement;
-        const savedTheme = localStorage.getItem('theme');
-        const systemPrefersDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
-        
-        if (savedTheme === 'dark' || (!savedTheme && systemPrefersDark)) {
-            html.classList.add('dark');
-        }
